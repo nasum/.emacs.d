40ffd114091082892d5c61b1225bbcd181eb243a
nasum <tomato.wonder.life@gmail.com>
2020-10-16 09:15:36 +0900

    emacsが自動的にinit.elに書き込む設定を別ファイルに保存するようにする

--- init.el
+++ init.el
@@ -1,2 +1,10 @@
 ;; add load path
 (add-to-list 'load-path "./elisp")
+
+;; カスタムファイルを別ファイルにする
+(setq custom-file (locate-user-emacs-file "custom.el"))
+;; カスタムファイルがない場合は作成する
+(unless (file-exists-p custom-file)
+  (write-region "" nil custom-file))
+;; カスタムファイルを読み込む
+(load custom-file)
