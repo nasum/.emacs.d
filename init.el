179b8b94c9d592126984eabd1c08f4d6514517c1
nasum <tomato.wonder.life@gmail.com>
2020-10-24 19:03:48 +0900

    projectileでivyを使うように修正

--- init.el
+++ init.el
@@ -123,10 +123,14 @@
   )
 (leaf bbatsov/projectile
   :el-get t
+  :bind
+  (
+   ("M-p f" . projectile--find-file)
+   ("M-p F" . projectile-switch-project)
+   )
   :config
   (projectile-mode +1)
-  (define-key projectile-mode-map (kbd "s-p") 'projectile-command-map)
-  (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
+  (setq projectile-completion-system 'ivy)
   )
 (leaf neotree
   :el-get t
